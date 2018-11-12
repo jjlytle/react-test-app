aeaf126e8133687aba3bc55d499050f431b443e8
Jeffrey Lytle <jjlytle@uw.edu>
2018-11-11 19:55:46 -0800

    test react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+// @ts-check
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Jeff', age: '35' },
+      { name: 'Sarah', age: '27' },
+      { name: 'Grandma', age: '84' },
+      { name: 'Kiki', age: '15' }
+    ]
+  }
+
+  switchButtonHandler = () => {
+    console.log('was clicked');
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hey, This is Jeff Lytle</h1>
+        <p>this is really working!!!/&#8221</p>
+        <button onClick={this.switchButtonHandler}>Switch</button>    
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
